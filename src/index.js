0ebb0aaa67dbc97b9bb59a1681c8a85ba7fe50bf
Farrukh Ehsan <farrukhehsan90@gmail.com>
2019-06-04 12:19:52 +0530

    Added Icons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import {Image} from 'react-native';
 import Decks from './components/decks/Decks';
 import AddDeck from './components/add-deck/AddDeck';
 
@@ -5,7 +6,9 @@ import {createBottomTabNavigator,createStackNavigator,createAppContainer} from '
 import AddCard from './components/add-card/AddCard';
 import Deck from './components/deck/Deck';
 import StartQuiz from './components/start-quiz/StartQuiz';
-
+import DecksIcon from './data-assets/decks-icon.png';
+import AddDeckIcon from './data-assets/add-deck-icon.png';
+// import Icon from '../assets/icon.png';
 
 const DecksNav=createStackNavigator({
     Decks:{screen:Decks},
@@ -25,8 +28,13 @@ const Tabs=createBottomTabNavigator({
             const {routeName}=navigation.state;
             let iconName;
             if(routeName==='Decks'){
-                
+                iconName=DecksIcon;
             }
+            else if(routeName==='AddDeck'){
+                iconName=DecksIcon;
+            }
+
+            return <Image source={iconName} style={{width:100 ,height:100}}/>
         }
 
     })
